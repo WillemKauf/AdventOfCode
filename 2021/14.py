441392c4e2850f10da5fcaed71e288d001256911
Willem Kaufmann <willem.kaufmann@gmail.com>
2021-12-14 01:23:28 -0500

    Cleaned up day 14

--- 2021/14.py
+++ 2021/14.py
@@ -23,17 +23,13 @@ def part_1(input_mol, input_dct):
                 input_mol = input_mol[:i+1] + append_str + input_mol[i+1:]
                 i += 1
             i += 1
-    least_common = Counter(input_mol).most_common()[-1][1]
-    most_common  = Counter(input_mol).most_common()[0][1]
-    return most_common - least_common
+
+    final_count = Counter(input_mol)
+    return max(final_count.values()) - min(final_count.values())
 
 def part_2(input_mol, input_dct):
-    count_dct = defaultdict(int)
     j = 0
-
-    while j < len(input_mol)-1:
-        count_dct[input_mol[j]+input_mol[j+1]] += 1
-        j += 1
+    count_dct = Counter([input_mol[i]+input_mol[i+1] for i in range(0, len(input_mol)-1)])
 
     for _ in range(0, 40):
         new_count_dct = defaultdict(int)
@@ -44,7 +40,6 @@ def part_2(input_mol, input_dct):
         count_dct = new_count_dct
 
     final_count_dct = defaultdict(int)
-
     for pair, num in count_dct.items():
         final_count_dct[pair[0]] += num
     final_count_dct[input_mol[-1]] += 1
