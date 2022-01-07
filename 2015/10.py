78687042da6c224766b6986cd7e30e333d3ae8ba
Willem Kaufmann <willem.kaufmann@gmail.com>
2022-01-07 12:30:23 -0500

    Made 10 nicer.

--- 2015/10.py
+++ 2015/10.py
@@ -4,7 +4,7 @@ def read_input():
             input_num = line.rstrip()
     return input_num
 
-def part_1(input_num):
+def look_say(input_num):
     input_list = (''.join(i + ('' if i == j else ', ')
             for i, j in zip(str(input_num), str(input_num)[1:] + str(input_num)[-1]))).split(", ")
     newNum = ""
@@ -12,21 +12,21 @@ def part_1(input_num):
         newNum += str(len(nums))+str(nums[0])
     return newNum
 
+def part_1(input_num):
+    for _ in range(0, 40):
+        input_num = look_say(input_num)
+    return len(input_num)
+
+
 def part_2(input_num):
-    return part_1(input_num)
+    for _ in range(0, 50):
+        input_num = look_say(input_num)
+    return len(input_num)
 
 def main():
-    input_num = read_input()
+    print(part_1(read_input()))
+    print(part_2(read_input()))
 
-    for _ in range(0, 40):
-        input_num = part_1(input_num)
-    print(len(input_num))
-
-    input_num = read_input()
-
-    for _ in range(0, 50):
-        input_num = part_1(input_num)
-    print(len(input_num))
 
 if __name__ == "__main__":
    main()
--- 2015/20.py
+++ 2015/20.py
@@ -5,16 +5,16 @@ def read_input():
             input_num = int(line.rstrip())
     return input_num
 
-def get_factors(house):
-    factors = set()
-    for i in range(1, int(house**1/2)+1):
-        if (house % i) == 0:
-            factors.add(i)
-            factors.add(house//i)
-    return factors
-
 def part_1(input_num):
-    house = 1
+    def get_factors(house):
+        factors = set()
+        for i in range(1, int(house**1/2)+1):
+            if (house % i) == 0:
+                factors.add(i)
+                factors.add(house//i)
+        return factors
+
+    house = 500000
     while True:
         factors      = get_factors(house)
         num_presents = 10*sum(factors)
@@ -24,11 +24,28 @@ def part_1(input_num):
     return None
 
 def part_2(input_num):
-    pass
+    def get_factors(house):
+        factors = set()
+        for i in range(1, int(house**1/2)+1):
+            if (house % i) == 0:
+                if house//i*50 >= house:
+                    factors.add(house//i)
+                if i*50 >= house:
+                    factors.add(i)
+        return factors
+    house = 786240
+    while True:
+        factors      = get_factors(house)
+        num_presents = 11*sum(factors)
+        if num_presents >= input_num:
+            return house
+        house += 1
+    return None
+
 
 def main():
     print(part_1(read_input()))
-    #print(part_2(read_input()))
+    print(part_2(read_input()))
 
 if __name__ == "__main__":
     main()
